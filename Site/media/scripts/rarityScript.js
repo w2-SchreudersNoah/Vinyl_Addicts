09df068a3a3115d40b1e64ac37774b5d89973013
w2-SchreudersNoah <noah.schreuders@edu-gmail.nl>
2019-02-21 14:06:03 +0100

    Noah - Zeldzaamheid checker v0.1

---
+++ Site/media/scripts/rarityScript.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $("#checkRarity").submit(function(){
+        var formResults = $(this).serializeArray(); 
+        var artistValue = 0;
+        var yearValue = formResults[1]["value"] * 0.1;
+        var collectorsEditionValue = 0;
+        var rarityScore; 
+        var waarde = "Geen Waarde";
+
+        switch(formResults[0]["value"]){
+            case 'Vader Abraham':      
+            case 'BZN':      
+            case 'Kamahl':      
+            case 'Heintje':   
+                artistValue = 4;
+                break;
+            case 'Supertramp':
+            case 'Queen':
+            case 'Metallica':
+            case 'Lionel Richie':
+                artistValue = 6;
+                break;
+            case 'The Beatles':
+            case 'Rolling Stones':
+            case 'Madonna':
+                artistValue = 7;
+                break;
+            default:
+                artistValue = 5;
+        };
+               
+        if(formResults.length === 3){
+             collectorsEditionValue = 5;
+        }
+        
+        rarityScore = (artistValue * yearValue) + collectorsEditionValue;
+        
+        if(rarityScore > 0 && rarityScore <= 10){
+            waarde = "Geen waarde";
+        }
+        else if(rarityScore > 10 && rarityScore <= 25){
+            waarde = "Gewoon";
+        }
+        else if(rarityScore > 25 && rarityScore <= 35){
+            waarde = "Zeldzaam";
+        }
+        else{
+            waarde = "Zeer zeldzaam";
+        }
+       
+    
+        return false;
+    });
+ });
